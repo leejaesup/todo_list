3d46067686113e86e649409f48527b3f6fe1f37e
Jaeseop Lee <leejaesup@naver.com>
2024-05-24 16:26:16 +0900

    백엔드 수정

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express =require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = process.env.MONGODB_URI_PROD;
+console.log("db = ", mongoURI);
 
 mongoose.connect(mongoURI, {useNewUrlParser:true})
     .then(() => {
@@ -17,7 +21,7 @@ mongoose.connect(mongoURI, {useNewUrlParser:true})
         console.log("DB connection fail", err);
     });
 
-const port = 5000;
-app.listen(port, () => {
+const port = 5005;
+app.listen(process.env.PORT || port, () => {
     console.log("server on ", port);
 })
