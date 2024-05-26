05e9019134b8f3a5c975950b3438ade61cc6d665
Jaeseop Lee <leejaesup@naver.com>
2024-05-26 11:46:05 +0900

    로그인 만들기 2

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -48,54 +48,9 @@ userController.loginWithEmail = async (req, res) => {
         }
 
         throw new Error('아이디 또는 비밀번호가 일치하지 않습니다.');
-    } catch (err) {
-        //400 Bad Request
-        res.status(400).json({status: "fail", error: err});
-    }
-}
-
-
-//READ(GET)
-userController.getTask = async (req, res) => {
-    try {
-        // const taskList = await Task.find({}).select("-_id -__v");
-        const taskList = await Task.find({}).select("-__v");
-
-        res.status(200).json({status: "ok", data: taskList});
-    } catch (err) {
-        res.status(400).json({status: "fail", error: err});
-    }
-}
-
-//UPDATE(PUT)
-userController.updateTask = async (req, res) => {
-    try {
-        const updateTask = await Task.findById(req.params.id);
-
-        if (!updateTask) {
-            return res.status(404).json({ status: "fail", error: "Task ID를 찾을 수 없습니다." });
-        }
-
-        const fields = Object.keys(req.body);
-
-        fields.map((item) => (
-            updateTask[item] = req.body[item]
-        ));
-        await updateTask.save();
-
-        res.status(200).json({status: "ok", data: updateTask});
-    } catch (err) {
-        res.status(400).json({status: "fail", error: err});
-    }
-}
-
-//DELETE(DELETE)
-userController.deleteTask = async (req, res) => {
-    try {
-        const deleteItem = await Task.findByIdAndDelete(req.params.id);
-        res.status(200).json({ status: "success", data: deleteItem });
     } catch (error) {
-        res.status(400).json({ status: "fail", error });
+        //400 Bad Request
+        res.status(400).json({status: "fail", message: error.message});
     }
 }
 
